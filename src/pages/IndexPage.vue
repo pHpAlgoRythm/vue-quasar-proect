745fe56d6f834fdc0f55462198bc0262acc469ff
Algorythm <124550715+pHpAlgoRythm@users.noreply.github.com>
2025-02-04 19:00:38 +0800

    second commit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,24 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="row">
+    <div class="col-12 col-md-4 col-lg-4">
+      <div class="text-h4 text-bold">Laptop Zone</div>
+      <div class="text-grey-7">Please Login Your Account Below</div>
+      <div class="column">
+        <q-input
+          v-model="form.email.value"
+          :error="form.email.error"
+          :error-message="form.email.msg"
+        >
+        </q-input>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
-});
+  name: 'IndexPage',
+})
 </script>
